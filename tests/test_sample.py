81df40e3f08c618815bce95355ee35a246a38480
Vladyslav <12333211233z@gmail.com>
2024-08-23 19:28:43 +0000

    Rework

---
+++ tests/test_sample.py
@@ -0,0 +1,9 @@
+# test_sample.py
+import unittest
+
+class SampleTest(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
